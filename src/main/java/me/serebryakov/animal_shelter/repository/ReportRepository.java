e2aa523bfb772d9d70602d51093a9f2d3d31e5a2
Serebryakov-A-E <117353355+Serebryakov-A-E@users.noreply.github.com>
2023-04-19 21:27:01 +0300

    Revert "Dev"

--- src/main/java/me/serebryakov/animal_shelter/repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package me.serebryakov.animal_shelter.repository;
-
-import me.serebryakov.animal_shelter.entity.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
