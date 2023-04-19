5b667ed05e408de4066d097479f00221c9684630
Serebryakov-A-E <117353355+Serebryakov-A-E@users.noreply.github.com>
2023-04-19 21:27:01 +0300

    Revert "Dev"

--- src/main/java/me/serebryakov/animal_shelter/exception/AnimalNotExistException.java
+++
@@ -1,7 +0,0 @@
-package me.serebryakov.animal_shelter.exception;
-
-public class AnimalNotExistException extends RuntimeException {
-    public AnimalNotExistException() {
-        super("Animal not exist!");
-    }
-}
