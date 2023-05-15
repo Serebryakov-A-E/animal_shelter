c8690d338bd9644863527e3c1e621dca255d7323
Serebryakov-A-E <117353355+Serebryakov-A-E@users.noreply.github.com>
2023-05-15 19:18:25 +0300

    Добавлена многопоточность в метод process и пул потоков, чтобы приходящие запросы пользователей обрабатывались одновременно

--- src/main/java/me/serebryakov/animal_shelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/me/serebryakov/animal_shelter/listener/TelegramBotUpdatesListener.java
@@ -2,26 +2,23 @@ package me.serebryakov.animal_shelter.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Contact;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.model.request.Keyboard;
-import com.pengrad.telegrambot.model.request.KeyboardButton;
-import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
-import com.pengrad.telegrambot.request.SendContact;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
-import me.serebryakov.animal_shelter.entity.Report;
 import me.serebryakov.animal_shelter.keyboard.TelegramKeyboard;
 import me.serebryakov.animal_shelter.service.AnimalService;
 import me.serebryakov.animal_shelter.service.OwnerService;
 import me.serebryakov.animal_shelter.service.ReportService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.task.support.ExecutorServiceAdapter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Component
 public class TelegramBotUpdatesListener implements UpdatesListener {
@@ -31,7 +28,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     private final ReportService reportService;
     private final AnimalService animalService;
     private final OwnerService ownerService;
-
+    private final ExecutorService executorService;
     private final TelegramKeyboard telegramKeyboard;
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot, ReportService reportService, AnimalService animalService, OwnerService ownerService, TelegramKeyboard telegramKeyboard) {
@@ -39,6 +36,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         this.reportService = reportService;
         this.animalService = animalService;
         this.ownerService = ownerService;
+        this.executorService = Executors.newFixedThreadPool(10);
         this.telegramKeyboard = telegramKeyboard;
     }
 
@@ -49,42 +47,23 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> list) {
-        try {
-            list.forEach(update -> {
-                logger.info("handles update: {}", update);
-                Message message = update.message();
-                /*
-                Contact contact = message.contact();
-                if (("/start").equals(message.text())) {
-                    {
-                        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup("");
-                        SendMessage sendMessage = new SendMessage(chatId, "sss").replyMarkup(replyKeyboardMarkup.resizeKeyboard(true));
-                        KeyboardButton keyboardButton = new KeyboardButton("CONTACTS").requestContact(true);
-                        replyKeyboardMarkup.addRow(keyboardButton);
-
-                        SendResponse sendResponse = telegramBot.execute(sendMessage);
-                        if (!sendResponse.isOk()) {
-                            logger.error("Error sending message: {}", sendResponse.description());
-                        }
+        executorService.submit(() -> {
+            try {
+                list.forEach(update -> {
+                    logger.info("handles update: {}", update);
+                    Message message = update.message();
+                    SendMessage sendMessage = telegramKeyboard.getResponse(message);
+                    SendResponse sendResponse = telegramBot.execute(sendMessage);
+                    if (!sendResponse.isOk()) {
+                        logger.error("Error sending message: {}", sendResponse.description());
                     }
-                } else if (message.text() == null && message.contact() != null) {
-                    System.out.println(contact.phoneNumber());
-                }
+                });
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
 
-
-                //SendResponse sendResponse = telegramBot.execute(sendContact);
-
-
-                 */
-                SendMessage sendMessage = telegramKeyboard.getResponse(message);
-                SendResponse sendResponse = telegramBot.execute(sendMessage);
-                if (!sendResponse.isOk()) {
-                    logger.error("Error sending message: {}", sendResponse.description());
-                }
-            });
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-        }
+        });
+        executorService.shutdown();
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 }
